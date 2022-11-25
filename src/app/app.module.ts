775958c502cc10187522d8fba80ae1a4b3028d74
ayeman <ayemanbsalauddin@gmail.com>
2022-11-25 15:45:45 +0600

    responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeroComponent
+    HeroComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
